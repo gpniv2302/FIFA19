86699f129c589909ef81c8704a8548fc03667d5a
gpniv2302 <gpniv2302@gamil.com>
2020-08-20 19:05:09 +0530

    Added "inputdata" in properties file

--- src/main/scala/DataIngestion/IngestPostgres.scala
+++ src/main/scala/DataIngestion/IngestPostgres.scala
@@ -12,12 +12,18 @@ import scala.io.Source.fromURL
 
 object IngestPostgres {
   def main(args: Array[String]): Unit = {
+    val reader = fromURL(getClass.getResource("Ingestion.properties")).bufferedReader()
+    val properties: Properties = new Properties()
+    properties.load(reader)
+    val jdbcurl = properties.getProperty("postgres.connection.url")
+    val table=properties.getProperty("postgres.tablename")
+
     val sparkconf = new SparkConf().setAppName("IngestPostgres").setMaster("local")
     val sparksession = SparkSession.builder().config(sparkconf).getOrCreate()
     val sqlcontext = sparksession.sqlContext
     import sqlcontext.implicits._
     val FIFAData = sqlcontext.read.format("csv").option("header", true).option("inferschema", true)
-      .load(args(0)).filter($"Club".isNotNull).filter($"Position".isNotNull)
+      .load(properties.getProperty("inputdata")).filter($"Club".isNotNull).filter($"Position".isNotNull)
       .select($"Overall",$"Position",$"Nationality",$"Name",$"Club",$"Wage",$"Value",$"Joined",$"Age")
     val FormatCols=FIFAData.withColumnRenamed("Joined","Date Joined")
       .withColumnRenamed("Wage","WageCurrency").withColumnRenamed("Value","ValueCurrency")
@@ -25,12 +31,6 @@ object IngestPostgres {
         .withColumn("Wage",regexp_replace($"WageCurrency","[€,M,K]","").cast("Decimal") * 1000)
       .withColumn("Value",regexp_replace($"ValueCurrency","[€,M,K]","").cast("Decimal") * 1000000)
       .select($"Overall".cast("Int"),$"Position",$"Nationality",$"Name",$"Club",$"Wage",$"Value",$"Joined",$"Age")
-
-      val reader = fromURL(this.asInstanceOf[Any].getClass.getResource("Ingestion.properties")).bufferedReader()
-      val properties: Properties = new Properties()
-      properties.load(reader)
-      val jdbcurl = properties.getProperty("postgres.connection.url")
-      val table=properties.getProperty("postgres.tablename")
       val connectionProperties = new Properties()
       connectionProperties.setProperty("Driver", "org.postgresql.Driver")
       connectionProperties.setProperty("user",properties.getProperty("postgres.username"))
